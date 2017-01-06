71629a273ac432217da28f0c5949c9a00a75a538
Jan Rohan <jan.rohan@firma.seznam.cz>
2017-01-06 11:24:21 +0100

    Bugfix for formanka

--- src/restaurants/formanka.js
+++ src/restaurants/formanka.js
@@ -15,16 +15,36 @@ module.exports = class Formanka extends Restaurant {
             let today = moment().format(', DD ');
             let counter = 0;
             let enable = false;
+
+            let days = [
+                'pondělí',
+                'úterý',
+                'středa',
+                'čtvrtek',
+                'pátek',
+            ];
+
+            let day = new Date().getDay() - 1;
+            if (day > days.length) {
+                resolve();
+                return;
+            }
+
             $('.bigbox .static .text p').each((i, elem) => {
 
                 let text = $(elem).text().trim();
+                let ltext = text.toLowerCase();
                 let html = $(elem).html().trim();
 
-                if (html.startsWith('<strong>')) {
+                if (html.startsWith('<strong>') && ltext.startsWith(days[day]) ) {
                     enable = true;
                     counter++;
                     if (counter == 1)
                         return;
+                }
+
+                if (!enable) {
+                    return;
                 }
 
                 if (counter > 3 || text.toLowerCase().startsWith('hlavní jídla')) {
@@ -34,7 +54,7 @@ module.exports = class Formanka extends Restaurant {
                 let line = text.replace(/\s\s+/g, ' ');
                 let lastSpaceIndex = line.lastIndexOf(' ');
                 let name = line.substring(0, lastSpaceIndex);
-                let price = line.substring(lastSpaceIndex);
+                let price = line.substring(lastSpaceIndex).replace('K', ' K');
                 if (text.length > 5) {
                     this.addItem(name, price);
                 }
